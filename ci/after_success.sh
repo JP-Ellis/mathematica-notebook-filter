26b6090e30c7649a7c2149a9c90b5bee911c7699
JP-Ellis <josh@jpellis.me>
2019-04-17 18:15:03 +1000

    Update CI to use new test suite
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,86 +1,44 @@
 #!/usr/bin/bash
 
-set -eux
-
-install_kcov() {
-    set -e
-    # Download and install kcov
-    wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz -O - | tar -xz
-    cd kcov-master
-    mkdir build
-    cd build
-    cmake ..
-    make -j$(nproc)
-    make install DESTDIR=../../kcov-build
-    cd ../..
-    rm -rf kcov-master
-    set +e
-}
+# Echo all commands before executing them
+set -o xtrace
+# Forbid any unset variables
+set -o nounset
+# Exit on any error
+set -o errexit
 
 run_kcov() {
     # Run kcov on all the test suites
-    for file in target/debug/mathematica_notebook_filter-*[^\.d]; do
-        mkdir -p "target/cov/$(basename $file)";
-        echo "Testing $(basename $file)"
-        ./kcov-build/usr/local/bin/kcov \
-            --exclude-pattern=/.cargo,/usr/lib\
-            --verify "target/cov/$(basename $file)" \
-            "$file";
-    done
-
-    # Run kcov with the binary and test various sets of arguments.
-    executable="target/debug/mathematica-notebook-filter"
-
-    mkdir -p "target/cov/exe-valid1"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-valid1" \
-        "$executable" -i "ci/test_notebook.nb" -o "ci/test_notebook_min.nb"
-
-    mkdir -p "target/cov/exe-valid2"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-valid2" \
-        "$executable" -v <"ci/test_notebook.nb" >"ci/test_notebook_min_pipe.nb"
-
-    mkdir -p "target/cov/exe-valid3"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-valid3" \
-        "$executable" -vv -i "ci/test_notebook.nb" -o "ci/test_notebook.nb"
-
-    # Also test a few invalid arguments
-    mkdir -p "target/cov/exe-invalid1"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-invalid1" \
-        "$executable" -vvv -i "ci/invalid.nb" -o "ci/new.nb"
-
-    mkdir -p "target/cov/exe-invalid2"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-invalid2" \
-        "$executable" -vvv --foobar
-
-    mkdir -p "target/cov/exe-invalid3"
-    ./kcov-build/usr/local/bin/kcov \
-        --exclude-pattern=/.cargo,/usr/lib \
-        --verify "target/cov/exe-invalid3" \
-        "$executable" -vvv <"ci/script.sh" >"/dev/null"
-
-    bash <(curl -s https://codecov.io/bash)
-    echo "Uploaded code coverage"
-}
-
-kcov_suite() {
-    if [[ "$TRAVIS_RUST_VERSION" == "stable" ]]; then
-        install_kcov
-        run_kcov
+    if [[ $COVERAGE_RUN != "true" ]]; then
+        cargo coveralls
+        COVERAGE_RUN=true
     fi
 }
 
+coverage_codecov() {
+    if [[ "$TRAVIS_RUST_VERSION" != "stable" ]]; then
+        return
+    fi
+
+    run_kcov
+
+    bash <(curl -s https://codecov.io/bash) -s target/kcov
+    echo "Uploaded code coverage to codecov.io"
+}
+
+coverage_coveralls() {
+    if [[ "$TRAVIS_RUST_VERSION" != "stable" ]]; then
+        return
+    fi
+
+    run_kcov
+
+    # Data is automatically uploaded by kcov
+}
+
 main() {
-    kcov_suite
+    coverage_coveralls
+    coverage_codecov
 }
 
 main
--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/bash
 
-set -eux
+# Echo all commands before executing them
+set -o xtrace
+# Forbid any unset variables
+set -o nounset
+# Exit on any error
+set -o errexit
 
 # Install clippy and rustfmt
 rustup_tools() {
     rustup component add clippy rustfmt
 }
 
-# Remove old builds from cache
-clean() {
-    find target -type f -name "mathematica-notebook-filter" -exec rm '{}' +
-    find target -type f -name "mathematica-notebook-filter-*" -exec rm '{}' +
+# Install cargo tools
+cargo_tools() {
+    cargo install cargo-update || echo "cargo-update already installed"
+    cargo install cargo-travis || echo "cargo-travis already installed"
+    # Update cached binaries
+    cargo install-update -a
 }
-
 
 main() {
     rustup_tools
-    clean
+    cargo_tools
 }
 
 main
